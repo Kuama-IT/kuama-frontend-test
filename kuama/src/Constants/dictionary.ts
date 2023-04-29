006604b96253cd37a1512ff98bf98765ab8d349a
Dario Milani <dmilani855@gmail.com>
2023-04-29 17:11:59 +0200

    finished

--- kuama/src/Constants/dictionary.ts
+++ kuama/src/Constants/dictionary.ts
@@ -1,5 +1,6 @@
 export const OVERVIEW_LABEL = "Overview";
 export const CURRENT_ACCOUNTS = "Current Accounts";
 export const OPEN_ACCOUNTS = "Open Accounts";
+export const COMPANY_NAME = "Kuama";
 
 export const IMAGE_LOGO = "https://media.licdn.com/dms/image/C4D0BAQE3mVAXLPx7tg/company-logo_200_200/0/1630936898091?e=2147483647&v=beta&t=o_l2vu5LJjh82bxQmZns9rfGedaJRyYZ6Ryr9g784wA";
--- kuama/src/PageStructure/PageStructure.tsx
+++ kuama/src/PageStructure/PageStructure.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../PageStructure/PageStructure.css";
-import { Layout, Menu, theme } from "antd";
+import { Layout, Menu, theme, Button, Checkbox, Form, Input } from "antd";
 import { SideBarLinks } from "../Utilities/Utility";
 import { Select } from "antd";
 import Data from "../data/details.json";
+import { COMPANY_NAME } from "../Constants/dictionary";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-const handleChange = (value: string) => {
-  console.log(`selected ${value}`);
-};
-
 export const PageSturcture = () => {
+  const [selectedElement, setSelectedElement] = useState<string>();
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const handleChange = (value: string) => {
+    setSelectedElement(value);
+  };
+
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
     <Layout>
       <Sider
@@ -46,7 +56,12 @@ export const PageSturcture = () => {
         />
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Header
+          className="header"
+          style={{ padding: 0, background: colorBgContainer }}
+        >
+          {`${COMPANY_NAME.toUpperCase()}`}
+        </Header>
         <Content style={{ margin: "14px 0 0" }}>
           <div
             style={{
@@ -73,10 +88,92 @@ export const PageSturcture = () => {
                 })}
               />
             </div>
+            <div className="forms">
+              {selectedElement === "individual" ? (
+                <div className="individual_form">
+                  <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{ maxWidth: 700 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                  >
+                    {Array.from(
+                      new Set(
+                        Data &&
+                          Data.details.map((item, index) => {
+                            return (
+                              <Form.Item
+                                label={Object.keys(item.fields)}
+                                rules={[
+                                  {
+                                    required: true,
+                                  },
+                                ]}
+                              >
+                                <Input />
+                              </Form.Item>
+                            );
+                          })
+                      )
+                    )}
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                      <Button type="primary" htmlType="submit">
+                        Submit
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                </div>
+              ) : (
+                <div className="company_form">
+                  <Form
+                    name="basic"
+                    labelCol={{ span: 8 }}
+                    wrapperCol={{ span: 16 }}
+                    style={{ maxWidth: 700 }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                  >
+                    {Array.from(
+                      new Set(
+                        Data &&
+                          Data.details.map((item, index) => {
+                            const labels = Object.keys(item.fields);
+                            return (
+                              <Form.Item
+                                label={`${labels}`}
+                                rules={[
+                                  {
+                                    required: true,
+                                  },
+                                ]}
+                              >
+                                <Input />
+                              </Form.Item>
+                            );
+                          })
+                      )
+                    )}
+
+                    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                      <Button type="primary" htmlType="submit">
+                        Submit
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                </div>
+              )}
+            </div>
           </div>
         </Content>
         <Footer className="footer_text" style={{ textAlign: "center" }}>
-          KUAMA
+          {`${COMPANY_NAME.toUpperCase()}`}
         </Footer>
       </Layout>
     </Layout>
